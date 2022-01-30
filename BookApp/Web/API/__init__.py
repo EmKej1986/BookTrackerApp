7cd0dcacabe411f53a24f8d8f0aec78b61284920
EmKej1986 <m.kopec1986@gmail.com>
2022-01-30 20:24:51 +0100

    Refactoring

--- BookApp/Web/API/__init__.py
+++ BookApp/Web/API/__init__.py
@@ -26,9 +26,3 @@ if __name__ == "__main__":
     app = create_app()
     app.debug = True
     app.run(host='0.0.0.0', port=5001)
-    # app.run(debug=True)
-    # app.run(host='127.0.0.1', port=5001)
-
-
-# TODO połączyć z database.sqlite3
-# TODO uruchomić API na 5001
